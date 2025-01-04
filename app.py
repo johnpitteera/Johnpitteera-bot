3a88cdc6121bc7513df5a7193bc169644d278b94
johnpitteera <johnpitteera@gmail.com>
2025-01-04 21:33:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Title of the app
-st.title("CJ's Chat aPP")
+st.title("JOHN P ITTEERA")
 
 # Initialize session state for chat history
 if "messages" not in st.session_state:
