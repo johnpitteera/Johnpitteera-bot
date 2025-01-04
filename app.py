0c92614e88397f428b2e68a274938448600702fe
johnpitteera <johnpitteera@gmail.com>
2025-01-04 20:37:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("CJ Bot")
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = openAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
